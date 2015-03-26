28e424b7ab7204d3d90e10111690c509803da3c9
Falken <johan.falkenstrom@gmail.com>
2015-03-26 06:18:53 +0100

    refact ongoing

--- node.py
+++ node.py
@@ -5,34 +5,9 @@ class NodeError(Exception):
      def __str__(self):
          return repr(self.error)
 
-class Node(object):
-    def __init__(self, name=""):
-        self.edge_list = []
-        self._tentative_weight = 0
-
-    def add_neighbour(self, end_node, cost=1, bi_directional=False):
-        self.edge_list.append(Edge(self, end_node, cost, bi_directional))
-
-    def neighbours(self):
-        for edge in self.edge_list:
-            yield edge.end_node
-
-    def edges(self):
-        return self.edge_list
-
-    def change_edge_costs(self, cost):
-        for edge in self.edge_list:
-            edge.cost = cost
-
-    def remove_neighbour(self, node):
-        (end_node, idx) = (None, None)
-        for idx, edge in enumerate(self.edge_list):
-            if edge.end_node == node:
-                (end_node, idx) = (edge.end_node, idx)
-                break
-        if end_node is None:
-            raise NodeError("Trying to remove a non existing neigbour")
-        self.edge_list.pop(idx)
+class Vertex(object):
+    def __init__(self, label=""):
+        pass
 
 class EdgeError(Exception):
      def __init__(self, value):
@@ -44,19 +19,8 @@ class EdgeError(Exception):
 class Edge(object):
     def __init__(self, cost, label=""):
         self._cost = cost
-        self.start_node = start_node
-        self.end_node = end_node
-        self.bi_directional = bi_directional
 
-    @property
-    def cost(self):
-        return self._cost
 
-    @cost.setter
-    def cost(self, new_cost):
-        if new_cost < 0:
-            raise EdgeError("Only positive edge costs are supported")
-        self._cost = new_cost
 
 class GraphError(Exception):
      def __init__(self, value):
--- test_node.py
+++ test_node.py
@@ -3,70 +3,6 @@
 import unittest
 from node import *
 
-class test_edge(unittest.TestCase):
-    def testDefaultCreate(self):
-        e = Edge(None, None, 3)
-        self.assertEqual(e.cost, 3)
-
-    def testCostProperty(self):
-        e = Edge(None, None, 3)
-        e.cost = 2
-        self.assertEqual(e.cost, 2)
-        with self.assertRaises(EdgeError):
-            e.cost = -1
-
-class test_cell(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def testBasicSetuo(self):
-        c1 = Node()
-        self.assertIsNotNone(c1)
-
-    def testAddDefaultNeighbourCostNode(self):
-        c1 = Node()
-        c2 = Node()
-        c1.add_neighbour(c2)
-        assert(c2 in c1.neighbours())
-        self.assertEquals(c1.edge_list[0].cost, 1)
-
-    def testAddNeighbourNode(self):
-        c1 = Node()
-        c2 = Node()
-        c1.add_neighbour(c2, 2)
-        assert(c2 in c1.neighbours())
-
-    def testRemoveNeigbourNode(self):
-         c1 = Node()
-         c2 = Node()
-         c3 = Node()
-         c1.add_neighbour(c2)
-         c1.add_neighbour(c3)
-         c1.remove_neighbour(c2)
-         assert(c2 not in c1.neighbours())
-         assert(c3 in c1.neighbours())
-         assert(c1 not in c2.neighbours())
-
-    def testRemoveAlreadyRemovedNode(self):
-         c1 = Node()
-         c2 = Node()
-         c1.add_neighbour(c2)
-         c1.remove_neighbour(c2)
-         with self.assertRaises(NodeError):
-             c1.remove_neighbour(c2)
-
-    def testCellNeighbourGenerator(self):
-         c1 = Node()
-         neighbour_nodes = []
-         for _ in range(10):
-             c = Node()
-             neighbour_nodes.append(c)
-             c1.add_neighbour(c)
-
-         for idx, neigbour in enumerate(c1.neighbours()):
-             self.assertEquals(neigbour, neighbour_nodes[idx])
-
 class test_graph(unittest.TestCase):
     def testGraphCreation(self):
         g = Graph()
@@ -76,7 +12,7 @@ class test_graph(unittest.TestCase):
 
     def testGraphAddAVertex(self):
         g = Graph()
-        n = Node()
+        n = Vertex()
         g.add_vertex(n)
         self.assertEquals(g._vertex_count, 1)
         self.assertTrue(n in g._vertices.keys())
@@ -84,7 +20,7 @@ class test_graph(unittest.TestCase):
 
     def testAddExistingVertexShouldRaiseGraphError(self):
         g = Graph()
-        n = Node()
+        n = Vertex()
         g.add_vertex(n)
         with self.assertRaises(GraphError):
             g.add_vertex(n)
@@ -92,7 +28,7 @@ class test_graph(unittest.TestCase):
 
     def testGraphRemoveVertex(self):
         g = Graph()
-        n = Node()
+        n = Vertex()
         g.add_vertex(n)
         g.rem_vertex(n)
         self.assertEquals(g._vertex_count, 0)
@@ -101,7 +37,7 @@ class test_graph(unittest.TestCase):
 
     def testRemoveNonExistingVertexShouldRaiseGraphError(self):
         g = Graph()
-        n = Node()
+        n = Vertex()
         g.add_vertex(n)
         g.rem_vertex(n)
         with self.assertRaises(GraphError):
@@ -110,8 +46,8 @@ class test_graph(unittest.TestCase):
 
     def testAddSeveralVerticesAndEdges(self):
         g = Graph()
-        n1 = Node("Node 1")
-        n2 = Node("Node 2")
+        n1 = Vertex("Node 1")
+        n2 = Vertex("Node 2")
         g.add_vertex(n1, n2)
         g.add_edge(n1, n2, Edge(2, "Test edge"))
         self.assertEquals(g._vertex_count, 2)
