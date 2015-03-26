fccde46fa6a4f56ed768d720c477074a96acb374
fa1k3n <johan.falkenstrom@gmail.com>
2015-03-26 14:31:26 +0100

    major updates in vertex code

--- node.py
+++ node.py
@@ -1,25 +1,6 @@
-class NodeError(Exception):
-     def __init__(self, value):
-         self.error = value
-
-     def __str__(self):
-         return repr(self.error)
-
-class Vertex(object):
-    def __init__(self, label=""):
-        pass
-
-class EdgeError(Exception):
-     def __init__(self, value):
-         self.error = value
-
-     def __str__(self):
-         return repr(self.error)
-
-class Edge(object):
-    def __init__(self, cost, label=""):
-        self._cost = cost
-
+import warnings
+import logging
+import collections
 
 
 class GraphError(Exception):
@@ -29,20 +10,45 @@ class GraphError(Exception):
      def __str__(self):
          return repr(self.error)
 
+class Vertex(object):
+    def __init__(self, label=""):
+        if isinstance(label, str):
+            self.label = label
+        else:
+            warnings.warn("Trying to add a non string label")
+
+class Edge(object):
+    def __init__(self, label="", cost=1):
+        self.cost = cost
+        self.label = label
+
 class Graph(object):
+    _GraphItem = collections.namedtuple('_GraphItem', ['vertex', 'edge'])
     def __init__(self, name=""):
         self.name = name
         self._vertex_count = 0
+        self._edge_count = 0
         self._vertices = {}
 
-    def add_vertex(self, *vert):
-        if vert not in self._vertices:
-            self._vertex_count += 1
-            self._vertices[vert] = []
-        else:
-            raise GraphError("Trying to add already existing node")
+    def add_vertex(self, *vert_args):
+        vert_list = list(vert_args)
+        new_vert = None
+        if len(vert_list) == 0:
+            new_vert = Vertex()
+            vert_list.append(new_vert)
 
-    def rem_vertex(self, vert):
+        for vert in vert_list:
+            if vert not in self._vertices:
+                self._vertex_count += 1
+                self._vertices[vert] = []
+            else:
+                raise GraphError("Trying to add already existing node")
+
+        return new_vert
+
+    def del_vertex(self, vert):
+        if isinstance(vert, str):
+            vert = self.vertex(vert)
         try:
             self._vertices.pop(vert)
         except:
@@ -52,6 +58,79 @@ class Graph(object):
 
     """
     should take start_ and end_ vertices both as references and as named nodes
+    if one of the vertices does not exists it should create it
     """
-    def add_edge(self, start_vert, end_vert, edge, directed=False):
-        pass
+    def add_edge(self, start_vert, end_vert, edge=None, directed=False):
+        self._edge_count += 1
+        try:
+            start_vert = self.vertex(start_vert)
+        except GraphError:
+            logging.info("str(start_vert)" + " created")
+            start_vert = Vertex(start_vert)
+            self.add_vertex(start_vert)
+        try:
+            end_vert = self.vertex(end_vert)
+        except GraphError:
+            logging.info(str(end_vert) + " created")
+            end_vert = Vertex(end_vert)
+            self.add_vertex(end_vert)
+
+        if edge == None:
+            edge = Edge()
+
+        self._vertices[start_vert].append(Graph._GraphItem(end_vert, edge))
+        if not directed:
+            self._vertices[end_vert].append(Graph._GraphItem(start_vert, edge))
+
+    def edge(self, label, vert_2=None):
+        if isinstance(label, str) and vert_2 == None:
+            return self._get_labeled_edge(label)
+        else:
+            vert_1 = self.vertex(label)
+            vert_2 = self.vertex(vert_2)
+            for graph_item in self._vertices[vert_1]:
+                if graph_item.vertex == vert_2:
+                    return graph_item.edge
+
+    def _get_labeled_edge(self, label):
+        for vert_key in self._vertices:
+            for graph_item in self._vertices[vert_key]:
+                if graph_item.edge.label == label:
+                    return graph_item.edge
+        raise GraphError("Label not found: " + str(label))
+
+    def vertex(self, vert):
+        if isinstance(vert, str):
+            return self._get_labeled_vertex(vert)
+        elif isinstance(vert, Vertex):
+            if vert in self._vertices.keys():
+                return vert
+        return GraphError("Vertex not found")
+
+    def _get_labeled_vertex(self, label):
+        for vert in self._vertices:
+            if vert.label == label:
+                return vert
+        raise GraphError("Label not found:" + str(label))
+
+    def __contains__(self, item):
+        if isinstance(item, str):
+            try:
+                self._get_labeled_vertex(item)
+            except:
+                pass
+            else:
+                return True
+        elif isinstance(item, Vertex):
+            return item in self._vertices
+        elif isinstance(item, tuple):
+            (start, end) = item
+            try:
+                self.edge(start, end)
+                self.edge(end, start)
+            except:
+                return False
+            else:
+                return True
+
+        return False
--- test_node.py
+++ test_node.py
@@ -12,10 +12,8 @@ class test_graph(unittest.TestCase):
 
     def testGraphAddAVertex(self):
         g = Graph()
-        n = Vertex()
-        g.add_vertex(n)
+        n = g.add_vertex()
         self.assertEquals(g._vertex_count, 1)
-        self.assertTrue(n in g._vertices.keys())
         self.assertEquals(len(g._vertices[n]), 0)
 
     def testAddExistingVertexShouldRaiseGraphError(self):
@@ -30,32 +28,70 @@ class test_graph(unittest.TestCase):
         g = Graph()
         n = Vertex()
         g.add_vertex(n)
-        g.rem_vertex(n)
+        g.del_vertex(n)
         self.assertEquals(g._vertex_count, 0)
         self.assertTrue(n not in g._vertices.keys())
         self.assertEquals(len(g._vertices.keys()), 0)
+
+    def testRemoveVertexShouldRemoveAllEdgesToIt(self):
+        g = Graph()
+        for i in range(3):
+            g.add_vertex(Vertex(str(i+1)))
+        g.add_edge("1", "2")
+        g.add_edge("2", "3")
+        g.add_edge("3", "1")
+        self.assertTrue(("1", "2") in g)
+        g.del_vertex("2")
+        self.assertFalse(("1", "2") in g)
+        self.assertFalse(("2", "1") in g)
+        self.assertFalse(("3", "2") in g)
+        self.assertTrue(("1", "3") in g)
+        self.assertEqual(g._edge_count, 1)
+
+
+    def testAddExistingVertexShouldRaiseGraphError(self):
+        g = Graph()
+        v = Vertex()
+        g.add_vertex(v)
+        with self.assertRaises(GraphError):
+            g.add_vertex(v)
 
     def testRemoveNonExistingVertexShouldRaiseGraphError(self):
         g = Graph()
         n = Vertex()
         g.add_vertex(n)
-        g.rem_vertex(n)
+        g.del_vertex(n)
         with self.assertRaises(GraphError):
-            g.rem_vertex(n)
+            g.del_vertex(n)
         self.assertEquals(g._vertex_count, 0)
 
     def testAddSeveralVerticesAndEdges(self):
         g = Graph()
-        n1 = Vertex("Node 1")
-        n2 = Vertex("Node 2")
-        g.add_vertex(n1, n2)
-        g.add_edge(n1, n2, Edge(2, "Test edge"))
+        v1 = Vertex("Vert 1")
+        v2 = Vertex("Vert 2")
+        g.add_vertex(v1, v2)
+        e1 = Edge("Edge 1", 2)
+        g.add_edge("Vert 1", v2, e1)
         self.assertEquals(g._vertex_count, 2)
         self.assertEquals(g._edge_count, 1)
-        self.assertTrue("Node 1" in g._vertices.keys())
-        self.assertTrue("Node 2" in g._vertices.keys())
-        self.assertTrue("Node 1" in g._vertices["Node 2"])
-        self.assertTrue("Node 2" in g._vertices["Node 1"])
+        # Check both ways to adress a vertex, both via reference and via name
+        self.assertTrue(v1 in g)
+        self.assertTrue("Vert 2" in g)
+        # Chaeck that they have been added to the graph correctly
+        self.assertTrue((v1, e1) in g._vertices[v2])
+        self.assertTrue((v2, e1) in g._vertices[v1])
+        # Check that the cost between the nodes are correct, test that the different
+        # test that the different adressing modes id/label works
+        self.assertTrue(g.edge("Edge 1").cost == 2)
+
+    def testAddSeveralVerticeasAndEdgesAgain(self):
+        g = Graph("My Graph")
+        g.add_vertex(Vertex("V1"), Vertex("V2"), Vertex("V3"))
+        g.add_edge("V1", "V4", Edge("V1<->V4", 2))
+        g.add_edge("V1", "V3", Edge("V1<->V3", 2))
+        g.add_edge("V2", "V3", Edge("V2<->V3", 4))
+
+        self.assertTrue(g.edge("V2<->V3").cost == 4)
 
 
 if __name__ == '__main__':
