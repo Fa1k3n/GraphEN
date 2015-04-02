fbc5e240b3c965b3de8e0f72c0a7a641bc1384f4
Falken <johan.falkenstrom@gmail.com>
2015-04-02 06:38:39 +0200

    improvements in drawing

--- display.py
+++ display.py
@@ -8,82 +8,46 @@ from graphen.algorithms.pathfinding import AStar as AStar
 
 class GridCanvas(Canvas, object):
     def __init__(self, root, grid, width=200, height=200):
-        Canvas.__init__(self, root, width=width, height=height)
+        Canvas.__init__(self, root, width=width, height=height, background="black")
         self.root = root
         self.grid = grid
         (self.vert_cells, self.hori_cells) = grid.size
         (self.cw, self.ch) = (width*1.0/self.vert_cells, height*1.0/self.hori_cells)
-        self._selected_cells = []
         self._edit_mode = False
-        self.path_objs = []
-        self.draw()
-
-    @property
-    def selected_cells(self):
-        return self._selected_cells
-
-    @selected_cells.setter
-    def selected_cells(self, cells):
-        self._selected_cells = cells
-        self.draw()
-
-    @property
-    def edit_mode(self):
-        return self._edit_mode
-
-    @edit_mode.setter
-    def edit_mode(self, em):
-        self._edit_mode = em
-        self.draw()
-
-    @property
-    def path_objs(self):
-        return self._path_objs
-
-    @path_objs.setter
-    def path_objs(self, po):
-        self._path_objs = po
-        self.draw()
-
-    def draw(self, path_obj=None):
         self.rect_ids = [[None]*self.hori_cells]*self.vert_cells
-
+        self.waypoints = {}
         for i in range(self.vert_cells):
             for j in range(self.hori_cells):
-                cell_fill = "white"
-                cell_active_fill = "blue"
-                if self.edit_mode:
-                    cell_active_fill = "black"
+                idx = self.create_rectangle(i * self.cw, j * self.ch, (i + 1) * self.cw, (j + 1) * self.ch,
+                                            fill = "white", activewidth=3, activeoutline="blue")
+                self.rect_ids[i][j]= idx
                 try:
                     c = g.get_cell(i, j)
                 except GraphError:
-                    # Cell does not exists
-                    cell_fill = "black"
-                    cell_active_fill = "black"
-                else:
-                   if len(list(filter(lambda path_obj: c in path_obj.visited(), self.path_objs))) > 0:
-                        cell_fill = "brown"
-                   #if len(list(filter(lambda path_obj: c in path_obj.fringe(), self.path_objs))) > 0:
-                   #    cell_fill = "gray"
-                   if (i, j) in self.selected_cells:
-                        cell_fill = "blue"
-                id = self.create_rectangle(i * self.cw, j * self.ch, (i + 1) * self.cw, (j + 1) * self.ch, fill = cell_fill, activefill=cell_active_fill)
-                self.rect_ids[i][j]= id
+                    self.itemconfig(idx, fill="black")
 
-        for po in self.path_objs:
-            for idx, start in enumerate(po.path):
-                try:
-                    end = po.path[idx + 1]
-                except Exception as e:
-                    pass
-                else:
-                    (sx, sy) = self.grid.cell_coord(start)
-                    (ex, ey) = self.grid.cell_coord(end)
-                    self.create_line(sx*self.cw + self.cw/2, sy*self.ch + self.ch/2,
-                                     ex*self.cw + self.cw/2, ey*self.ch + self.ch/2, width=3)
+    def process_click(self, x, y):
+        colors = {"black" : "white", "white" : "black"}
+        idx = self.find_closest(x, y)
+        print idx
+        fill = self.itemcget(idx, "fill")
+        self.itemconfig(idx, fill=colors[fill])
 
-    def get_rect(self, x, y):
-        return self.rect_ids[y][x]
+    def cell_to_screen(self, cell):
+        return (cell[0]*self.cw+self.cw/2, cell[1]*self.ch+self.ch/2)
+
+    def toggle_waypoint(self, cell):
+        try:
+            self.delete(self.waypoints[cell])
+        except KeyError:
+            (x, y) = self.cell_to_screen(cell)
+            idx = self.create_rectangle(x-self.cw/2+2, y-self.ch/2+2, x+self.cw/2-2, y+self.ch/2-2, fill="red", activeoutline="yellow")
+            self.waypoints[cell] = idx
+
+    def draw_path(self, po):
+        coords = [(x*self.cw + self.cw/2, y*self.ch+self.ch/2) for x,y in [self.grid.cell_coord(seg) for seg in po.path]]
+        coords = list(sum(coords, ()))
+        self.create_line(*coords, width=3, smooth=True)
 
 class GridController(object):
     def __init__(self, grid, view):
@@ -117,20 +81,22 @@ class GridController(object):
             return AStar(self.grid, self.grid.dist)
 
     def mouse_click(self, event):
-        cell = self.coord_to_cell(event.x, event.y)
+        cell = (rect_x, rect_y) = self.coord_to_cell(event.x, event.y)
         d = self.create_path_obj()
         cell_lbl = Grid.CellCoordLabel(cell[0], cell[1])
         # See if cell is existing
         try:
             c = self.grid.vertex(cell_lbl)
         except:
-            print "here", cell_lbl
-            self.grid.add_cell(cell[0], cell[1])
-            self.view.draw()
+            if self.map_edit:
+                self.view.process_click(event.x, event.y)
+                self.grid.add_cell(cell[0], cell[1])
             return
         else:
             if self.map_edit:
+                self.view.process_click(event.x, event.y)
                 self.grid.del_cell(cell)
+
                 # Create all PO's again, world has changed
                 new_pos = []
                 for po in self.view.path_objs:
@@ -141,24 +107,26 @@ class GridController(object):
                 self.view.path_objs = new_pos
                 return
 
+        self.view.toggle_waypoint(cell)
+
         if cell in self.selected_cells:
             idx = self.selected_cells.index(cell)
             self.selected_cells.remove(cell)
 
             po_to_be_removed = []
             (new_start, new_end) = (None, None)
-            for po in self.view.path_objs:
-                (po_start, po_end) = (self.grid.cell_coord(po.path[0]), self.grid.cell_coord(po.path[-1]))
-                if po_start == cell:
-                    po_to_be_removed.append(po)
-                    new_end = po_end
-                elif po_end == cell:
-                    po_to_be_removed.append(po)
-                    new_start = po_start
-            [self.view.path_objs.remove(po) for po in po_to_be_removed]
-            if new_end != None and new_start != None:
-                d.shortest_path(self.grid.get_cell(new_start), self.grid.get_cell(new_end))
-                self.view.path_objs.append(d)
+#            for po in self.view.path_objs:
+ #               (po_start, po_end) = (self.grid.cell_coord(po.path[0]), self.grid.cell_coord(po.path[-1]))
+ #               if po_start == cell:
+ #                   po_to_be_removed.append(po)
+ #                   new_end = po_end
+ #               elif po_end == cell:
+ #                   po_to_be_removed.append(po)
+ #                   new_start = po_start
+ #           [self.view.path_objs.remove(po) for po in po_to_be_removed]
+ #           if new_end != None and new_start != None:
+ #               d.shortest_path(self.grid.get_cell(new_start), self.grid.get_cell(new_end))
+ #               self.view.path_objs.append(d)
         else:
             self.selected_cells.append(cell)
             idx = self.selected_cells.index(cell)
@@ -167,7 +135,9 @@ class GridController(object):
                 e = self.grid.get_cell(self.selected_cells[idx][0], self.selected_cells[idx][1])
                 d.shortest_path(s, e)
                 print ("Time", d.time)
-                self.view.path_objs.append(d)
+                #self.view.path_objs.append(d)
+                self.view.draw_path(d)
+
 
         self.view.selected_cells = self.selected_cells
 
@@ -189,7 +159,7 @@ if __name__ == '__main__':
             g.del_vertex(Grid.CellCoordLabel(hori_cells/i - x - 1, vert_cells/i))
 
     root = Tk()
-    w = GridCanvas(root, g, width=400, height=400)
+    w = GridCanvas(root, g, width=600, height=600)
     c = GridController(g, w)
 
     w.pack()
