a2b7bb7ce8a5b20887a58df852dcebf15be54f86
ejohfal <johan.falkenstrom@gmail.com>
2015-04-02 13:23:26 +0200

    updates yet again

--- display.py
+++ display.py
@@ -6,6 +6,33 @@ import warnings
 from graphen.algorithms.pathfinding import Djikstra as Djikstra
 from graphen.algorithms.pathfinding import AStar as AStar
 
+class Waypoint(object):
+    def __init__(self, cell):
+        self.cell = cell
+        self.next_wp = None
+        self.prev_wp = None
+
+    def create(self, view):
+        (x, y) = view.cell_to_screen(self.cell)
+        idx = view.create_rectangle(x-view.cw/2+2, y-view.ch/2+2, x+view.cw/2-2, y+view.ch/2-2, fill="red", activeoutline="yellow")
+        return idx
+
+class Path(object):
+    def __init__(self, PO, start, end):
+        self.start_wp = start
+        self.end_wp = end
+        self.start_vertex = PO._graph.get_cell(start.cell)
+        self.end_vertex = PO._graph.get_cell(end.cell)
+        self.po = PO
+        self.po.shortest_path(self.start_vertex, self.end_vertex)
+        print ("Time", self.po.time)
+
+    def create(self, view):
+        coords = [(x*view.cw + view.cw/2, y*view.ch+view.ch/2) for x,y in [view.grid.cell_coord(seg) for seg in self.po.path]]
+        coords = list(sum(coords, ()))
+        idx = view.create_line(*coords, width=3, smooth=True)
+        return idx
+
 class GridCanvas(Canvas, object):
     def __init__(self, root, grid, width=200, height=200):
         Canvas.__init__(self, root, width=width, height=height, background="black")
@@ -15,7 +42,7 @@ class GridCanvas(Canvas, object):
         (self.cw, self.ch) = (width*1.0/self.vert_cells, height*1.0/self.hori_cells)
         self._edit_mode = False
         self.rect_ids = [[None]*self.hori_cells]*self.vert_cells
-        self.waypoints = {}
+        self.objects = {}
         for i in range(self.vert_cells):
             for j in range(self.hori_cells):
                 idx = self.create_rectangle(i * self.cw, j * self.ch, (i + 1) * self.cw, (j + 1) * self.ch,
@@ -26,33 +53,30 @@ class GridCanvas(Canvas, object):
                 except GraphError:
                     self.itemconfig(idx, fill="black")
 
-    def process_click(self, x, y):
-        colors = {"black" : "white", "white" : "black"}
-        idx = self.find_closest(x, y)
-        print idx
-        fill = self.itemcget(idx, "fill")
-        self.itemconfig(idx, fill=colors[fill])
-
     def cell_to_screen(self, cell):
         return (cell[0]*self.cw+self.cw/2, cell[1]*self.ch+self.ch/2)
 
-    def toggle_waypoint(self, cell):
-        try:
-            self.delete(self.waypoints[cell])
-        except KeyError:
-            (x, y) = self.cell_to_screen(cell)
-            idx = self.create_rectangle(x-self.cw/2+2, y-self.ch/2+2, x+self.cw/2-2, y+self.ch/2-2, fill="red", activeoutline="yellow")
-            self.waypoints[cell] = idx
+    def add(self, object):
+        idx = object.create(self)
+        self.objects[object] = idx
 
-    def draw_path(self, po):
-        coords = [(x*self.cw + self.cw/2, y*self.ch+self.ch/2) for x,y in [self.grid.cell_coord(seg) for seg in po.path]]
-        coords = list(sum(coords, ()))
-        self.create_line(*coords, width=3, smooth=True)
+    def remove(self, object):
+        self.delete(self.objects[object])
+
+    def process_click(self, x, y):
+        colors = {"black" : "white", "white" : "black"}
+        idx = self.find_closest(x, y)
+        fill = self.itemcget(idx, "fill")
+        self.itemconfig(idx, fill=colors[fill])
 
 class GridController(object):
     def __init__(self, grid, view):
         self.grid = grid
         self.view = view
+        self.objects = {}
+        self.waypoints = {}
+        self.last_added_wp = None
+        self.paths = {}
         self.view.bind("<Button-1>", self.mouse_click)
         self.view.root.bind("<a>", self.toggle_algo)
         self.view.root.bind("<e>", self.toggle_map_edit)
@@ -61,6 +85,7 @@ class GridController(object):
         self.map_edit = False
         self.set_win_title()
         self.selected_cells = []
+
 
     def set_win_title(self):
         self.view.root.wm_title("Algorithm [" + self.algo + "] Map edit [" + str(self.map_edit) + "]")
@@ -74,11 +99,31 @@ class GridController(object):
         self.view.edit_mode = self.map_edit
         self.set_win_title()
 
+    def toggle_waypoint(self, cell):
+        try:
+            self.view.remove(self.objects[cell])
+            wp = self.objects.pop(cell)
+        except KeyError:
+            wp = Waypoint(cell)
+            self.objects[cell] = wp
+            self.view.add(wp)
+            self.waypoints[wp] = {"prev":None, "next":None, "paths":[]}
+            return (True, wp)
+        return (False, wp)
+
     def create_path_obj(self):
         if self.algo == "Djikstra":
             return Djikstra(self.grid)
         else:
             return AStar(self.grid, self.grid.dist)
+
+    def update_paths(self):
+        for wp in self.paths:
+            p = self.paths[wp]
+            self.view.remove(p)
+            p.po = self.create_path_obj()
+            p.po.shortest_path(p.start_vertex, p.end_vertex)
+            self.view.add(p)
 
     def mouse_click(self, event):
         cell = (rect_x, rect_y) = self.coord_to_cell(event.x, event.y)
@@ -91,55 +136,56 @@ class GridController(object):
             if self.map_edit:
                 self.view.process_click(event.x, event.y)
                 self.grid.add_cell(cell[0], cell[1])
+                self.update_paths()
             return
         else:
             if self.map_edit:
                 self.view.process_click(event.x, event.y)
                 self.grid.del_cell(cell)
+                self.update_paths()
 
-                # Create all PO's again, world has changed
-                new_pos = []
-                for po in self.view.path_objs:
-                    p = self.create_path_obj()
-                    (start, end) = (po.path[0], po.path[-1])
-                    p.shortest_path(start, end)
-                    new_pos.append(p)
-                self.view.path_objs = new_pos
                 return
 
-        self.view.toggle_waypoint(cell)
-
-        if cell in self.selected_cells:
-            idx = self.selected_cells.index(cell)
-            self.selected_cells.remove(cell)
-
-            po_to_be_removed = []
-            (new_start, new_end) = (None, None)
-#            for po in self.view.path_objs:
- #               (po_start, po_end) = (self.grid.cell_coord(po.path[0]), self.grid.cell_coord(po.path[-1]))
- #               if po_start == cell:
- #                   po_to_be_removed.append(po)
- #                   new_end = po_end
- #               elif po_end == cell:
- #                   po_to_be_removed.append(po)
- #                   new_start = po_start
- #           [self.view.path_objs.remove(po) for po in po_to_be_removed]
- #           if new_end != None and new_start != None:
- #               d.shortest_path(self.grid.get_cell(new_start), self.grid.get_cell(new_end))
- #               self.view.path_objs.append(d)
+        (wp_added, wp) = self.toggle_waypoint(cell)
+        if wp_added:
+            try:
+                self.waypoints[self.last_added_wp]["next"] = wp
+                self.waypoints[wp]["prev"] = self.last_added_wp
+            except KeyError as e:
+                pass
+            else:
+                prev_wp = self.waypoints[wp]["prev"]
+                p = Path(d, prev_wp, wp)
+                self.view.add(p)
+                self.paths[wp] = p
+                if p not in self.waypoints[wp]["paths"]:
+                    self.waypoints[wp]["paths"].append(p)
+                if p not in self.waypoints[prev_wp]["paths"]:
+                    self.waypoints[prev_wp]["paths"].append(p)
+            self.last_added_wp = wp
         else:
-            self.selected_cells.append(cell)
-            idx = self.selected_cells.index(cell)
-            if idx > 0:
-                s = self.grid.get_cell(self.selected_cells[idx-1][0], self.selected_cells[idx-1][1])
-                e = self.grid.get_cell(self.selected_cells[idx][0], self.selected_cells[idx][1])
-                d.shortest_path(s, e)
-                print ("Time", d.time)
-                #self.view.path_objs.append(d)
-                self.view.draw_path(d)
+            prev_wp = self.waypoints[wp]["prev"]
+            next_wp = self.waypoints[wp]["next"]
+            for path in self.waypoints[wp]["paths"]:
+                self.view.remove(path)
+            self.waypoints.pop(wp)
+            try:
+                self.waypoints[prev_wp]["next"] = next_wp
+            except:
+                return
+            try:
+                self.waypoints[next_wp]["prev"] = prev_wp
+            except:
+                self.last_added_wp = prev_wp
+                return
+            p = Path(d, prev_wp, next_wp)
+            self.add_path_to_wp(p, prev_wp)
+            self.add_path_to_wp(p, next_wp)
+            self.view.add(p)
 
-
-        self.view.selected_cells = self.selected_cells
+    def add_path_to_wp(self, path, waypoint):
+        if path not in self.waypoints[waypoint]["paths"]:
+            self.waypoints[waypoint]["paths"].append(path)
 
     def coord_to_cell(self, x, y):
         cell_x = int(x / self.view.cw)
